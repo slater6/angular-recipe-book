1a9f6d7532c7bed4715f29dcf3533b31648ebf23
Bryan Rodrigues <slaters@gmail.com>
2016-10-12 00:08:42 -0400

    Added Recipe Functionality, Shopping List Skeleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { HeaderComponent} from './header.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Yo app works!';
+	title = 'Yo app works!';
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Recipe } from '../recipe';
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+	selector: 'app-recipe-list',
+	templateUrl: './recipe-list.component.html',
+	styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+	recipes: Recipe[] = [];
+	@Output() recipeSelected = new EventEmitter<Recipe>();
+	recipe = new Recipe('Lorem', 'Lorem description', 'http://placehold.it/75x75')
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
+
+	onSelected(recipe: Recipe) {
+		this.recipeSelected.emit(recipe);
+	}
 
 }
