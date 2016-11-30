63cbf320cd14b4589e980301f0b6fe5468f0f9cd
Bryan Rodrigues <slaters@gmail.com>
2016-11-29 22:41:32 -0500

    Refactored selectedRecipe to be assigned by router params

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -4,21 +4,25 @@ import {Recipe} from "./recipe";
 
 @Injectable()
 export class RecipeService {
-  private recipes: Recipe[] = [
-    new Recipe('Schnitzel', 'Very tasty', 'http://images.derberater.de/files/imagecache/456xXXX_berater/berater/slides/WienerSchnitzel.jpg', [
-      new Ingredient('French Fries', 2),
-      new Ingredient('Pork Meat', 1)
-    ]),
-    new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [
-      new Ingredient('Romaine Lettuce', 2),
-      new Ingredient('Watermelon', 1),
-    ])
-  ];
+	private recipes: Recipe[] = [
+		new Recipe('Schnitzel', 'Very tasty', 'http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-5.jpg', [
+			new Ingredient('French Fries', 2),
+			new Ingredient('Pork Meat', 1)
+		]),
+		new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [
+			new Ingredient('Romaine Lettuce', 2),
+			new Ingredient('Watermelon', 1),
+		])
+	];
 
-  constructor() { }
+	constructor() { }
 
-  getRecipes() {
-    return this.recipes;
-  }
+	getRecipes() {
+		return this.recipes;
+	}
+
+	getRecipe(id: number) {
+		return this.recipes[id];
+	}
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -3,17 +3,17 @@ import { Ingredient } from '../shared';
 import { ShoppingListService } from './shopping-list.service';
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+	selector: 'app-shopping-list',
+	templateUrl: './shopping-list.component.html',
+	styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  items: Ingredient[] = [];
+	items: Ingredient[] = [];
 
-  constructor(private shoppingListService: ShoppingListService) { }
+	constructor(private shoppingListService: ShoppingListService) { }
 
-  ngOnInit() {
-    this.items = this.shoppingListService.getItems();
-  }
+	ngOnInit() {
+		this.items = this.shoppingListService.getItems();
+	}
 
 }
